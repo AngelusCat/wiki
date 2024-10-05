27ad0f6ec4020d654414827826038e40332a0b24
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-10-05 09:32:19 +0300

    Article

--- app/Entities/Article.php
+++ app/Entities/Article.php
@@ -2,7 +2,54 @@
 
 namespace App\Entities;
 
+use App\Services\WordsAtomsTDG;
+use Illuminate\Support\Collection;
+
 class Article
 {
+    private string $title;
+    private Collection $wordsAtoms;
+    private string $content;
 
+    public function __construct(string $title, string $content, Collection $wordsAtoms = new Collection())
+    {
+        $this->title = $title;
+        $this->wordsAtoms = ($wordsAtoms->isEmpty()) ? $this->parseContentIntoWordsAtoms() : $wordsAtoms;
+        $this->content = $this->removeAccents($content);
+    }
+
+    private function removeAccents(string $content): string
+    {
+        $replacementArray = [
+            "А́" => "А",
+            "а́" => "а",
+            "Е́" => "Е",
+            "е́" => "е",
+            "И́" => "И",
+            "и́" => "и",
+            "О́" => "О",
+            "о́" => "о",
+            "У́" => "У",
+            "у́" => "у",
+            "Ы́" => "Ы",
+            "ы́" => "ы",
+            "Э́" => "Э",
+            "э́" => "э",
+            "Ю́" => "Ю",
+            "ю́" => "ю",
+            "Я́" => "Я",
+            "я́" => "я"
+        ];
+        return strtr($content, $replacementArray);
+    }
+
+    public function getLink(): string
+    {
+        return "https://ru.wikipedia.org/wiki/" . $this->title;
+    }
+
+    private function parseContentIntoWordsAtoms(): Collection
+    {
+        //
+    }
 }

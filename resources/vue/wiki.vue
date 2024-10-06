f66fb8401cedaf0ef60cfef95bc24bf291f4f19f
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-10-06 07:52:46 +0300

    ...

--- resources/vue/wiki.vue
+++ resources/vue/wiki.vue
@@ -7,6 +7,15 @@
         searchShow.value = !searchShow.value;
     }
 
+    function send(e)
+    {
+        e.preventDefault();
+        let response = fetch("http://wiki/import", {
+            method: "POST",
+            body: new FormData(e.currentTarget)
+        });
+    }
+
     let importShow = ref(true);
     let searchShow = ref(false);
 </script>
@@ -16,6 +25,12 @@
     <button @click="changeVisibility">Search</button>
     <div v-if="importShow">
         <p>{{ "import" }}</p>
+        <form action="/import" method="post" @submit="send">
+<!--            add csrf-->
+            <input type="text" name="title" placeholder="ключевое слово">
+            <button type="submit">Скопировать</button>
+        </form>
+
     </div>
     <div v-if="searchShow">
         <p>{{ "search" }}</p>
